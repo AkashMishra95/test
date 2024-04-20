99f1d7e21c1e3ca09f5af973a59c3ecd7b64d223
Akash <152004685+AkashMishra95@users.noreply.github.com>
2024-04-20 20:22:44 +0530

    bla

---
+++ test/src/content_option.js
@@ -0,0 +1,137 @@
+const logotext = "Akash ";
+const meta = {
+    title: "Akash Mishra",
+    description: "I’m Akash Mishra , work as Cloud Infra Engineer in India",
+};
+
+const introdata = {
+    title: " Hi I’m Akash Mishra",
+    animated: {
+        first: " I am an Author",
+        second: "I work in IT ",
+        third: " I am an Artist ",
+    },
+    description: "Am an author lives in Pune , who find himself fall in between work & art",
+    your_img_url: "https://i.postimg.cc/tT8hWBSV/Whats-App-Image-2024-04-18-at-12-10-58-AM.jpg",
+};
+
+const dataabout = {
+    title: "A bit about my self ",
+    aboutme: "Akash Mishra is currently working as a Senior Infrastructure Engineer. He has spent most of his life in Lucknow and is currently based in Pune. He is a graduate from City Academy Degree College in Computer Application and Public Administration. His 4 years of IT career started with HCL and is currently going fine with LTIMindtree. Akash is a big time foody. His love for food and workout is unbeatable. He loves sketching in his free time. Akash’s love for writing started with writing lyrics.",
+};
+const worktimeline = [{
+    jobtitle: "Senior Cloud Infra Engineer",
+    where: "LTI Mindtree , Pune",
+    date: "Feb 2024 - Present",
+},{
+        jobtitle: "Senior Infrastructure Engineer",
+        where: "Nium , Bangalore",
+        date: "Dec 2022-Oct 2023",
+    },
+    {
+        jobtitle: "Senior Technical Advisor I",
+        where: "Concentrix , Pune",
+        date: "July 2021 - Sep 2022",
+    },
+    {
+        jobtitle: "Senior Customer Exceutive",
+        where: "HCL Tech , Lucknow",
+        date: "january 2019 - April 2021",
+    },
+];
+
+const skills = [{
+        name: "Azure",
+        value: 85,
+    },
+    {
+        name: "AWS",
+        value: 85,
+    },
+    {
+        name: "O365",
+        value: 80,
+    },
+    {
+        name: "ZScaler",
+        value: 80,
+    },
+    {
+        name: "Jenkins",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Senior Infrastructure Engineer",
+        description: " Infrastructure and Production Support  tools O365, Jira, Sophos, Patch Manager, Zscaler, AWS, Azure, Azure Active Directory, Zoom, Slack, Sprinto, Abnormal Security, Hexnode, Okta cloud, Freshdesk",
+    },
+    {
+        title: "Senior Technical Advisor I",
+        description: " Microsoft O365 Microsoft 365 Support Engineer Troubleshooting and resolving cases related to Hyper V, VMware IP/Networking/ DNS/ DHCP.MFA/ SSO/ ADFS/ PHS/ PTA. PowerShell scripting. ",
+    },
+    {
+        title: "Senior Customer Exceutive",
+        description: "Assisting customers as per their issues regarding Wi-Fi, Mobile, TV and Telephone Online certification examination – Both greeting and proctoring and resolving technical, backend apps and networking related issues during ongoing exams.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.postimg.cc/9QGggS1Q/UF-cover-Page-1-page-0001.jpg",
+        description: "This book narrates about two people who are entangled with each other because of a misunderstanding which leads them in a very complicated life situations. Situations that are going to define their characters and also their lives.",
+        link: "https://notionpress.com/read/unique-friend#ebook",
+    },
+    {
+        img: "https://i.postimg.cc/3xRCmc6q/png.png",
+        description: "This book delves into the life of Akash, a talented musician, and explores his complex relationship with his father. This story reveals the incredible sacrifices made by both a father and son, highlighting the strength of their bond.         ",
+        link: "https://notionpress.com/read/deal",
+    },
+    {
+        img: "https://i.postimg.cc/sg4dG1km/The-promise-cover-page.png",
+        description: "This book gives us insight into Kaynaat and Aryan’s love story. This story details Aryan’s journey from a librarian to a successful and famous author and the role of Kaynaat in his life.        ",
+        link: "https://notionpress.com/read/the-promise-1398936",
+    },
+    {
+        img: "https://i.postimg.cc/zf4LLtZT/No-cheating-cover-page-405-KB.jpg   ",
+        description: "The focus of this story revolves around Aryan and his wife, Maya. Despite marrying out of love, their marriage has lost its love and is now filled with doubts. ",
+        link: "https://notionpress.com/read/no-cheating",
+    },
+
+    {
+        img: "https://i.postimg.cc/PxcBnDDX/Pretty-Attachments-cover-page-205-KB.jpg",
+        description: "This anthology contains four unique short stories. These stories differ in the feelings and emotions they evoke. This series of short stories will explore a range of themes, including self-love, friendship, revenge, acceptance, family values, and, above all, the power of true love. ",
+        link: "https://notionpress.com/read/pretty-attachments  ",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: " akashthegreatmishra@gmail.com",
+    YOUR_FONE: "+91 8738873370",
+    description: "Looking forward to collaborating in the future. Feel free to contact me here or submit your details. Cheers!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_86167kd",
+    YOUR_TEMPLATE_ID: "template_piedg4e",
+    YOUR_USER_ID: "2fFMTCuyJJ4WMIAvM",
+};
+
+const socialprofils = {
+    github: "https://github.com/AkashMishra95",
+    facebook: "https://facebook.com/akashthegreatmishra",
+    linkedin: "https://www.linkedin.com/in/akash-mishra-08b670190",
+    twitter: "https://twitter.com/AkashMishra_95",
+    instagram: "https://www.instagram.com/akash_the_great_1?utm_source=qr&igsh=MWFhOG00Y2dzbjNqNA==",
+    telegram:"https://t.me/theakashmishraauthor"
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
